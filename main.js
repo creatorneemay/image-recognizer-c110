e0c937182c81849f415dd76522d6440892b254f1
creatorneemay <72016905+creatorneemay@users.noreply.github.com>
2021-02-06 16:43:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,26 @@ Webcam.set({
 });
 camera=document.getElementById("camera")
 Webcam.attach("camera")
+function takesnapshot(){
+    Webcam.snap(function(image_url){
+        document.getElementById("camera_result").innerHTML="<img id='cap_img' src='"+image_url+"'>";
+    });
+}
+console.log("ml5version",ml5.version);
+classifier=ml5.imageClassifier("MobileNet",modelloaded);
+function modelloaded(){
+    console.log("model loaded");
+}
+function identifyimage(){
+    img=document.getElementById("cap_img");
+    classifier.classify(img, getresult);
+}
+function getresult(error,result){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(result);
+        document.getElementById("object_name").innerHTML=result[0].label;
+    }
+}
